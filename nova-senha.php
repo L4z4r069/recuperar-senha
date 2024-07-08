66b91bfbb9bef9f98c674eb900f5377e6b5c32dd
Lázaro <lazaro.2022315968@aluno.iffar.edu.br>
2024-07-08 17:03:29 -0300

    terminando o nova sneha

--- nova-senha.php
+++ nova-senha.php
@@ -5,5 +5,28 @@
 $email = $_GET['email'];
 $token = $_GET['token'];
 
-
-?>
+require_once "conexao.php";
+$conexao = conectar();
+$sql = "SELECT * FROM `recuperar-senha` WHERE email='$email' AND token='$token'";
+$resultado = executarSQL($conexao, $sql);
+$recuperar = mysqli_fetch_assoc($resultado);
+if ($resultado == null) {
+    echo "Email ou token incorretos. Tente fazer um novo pedido
+    de recuperação de senha.";
+    die();
+} else {
+    // verificar a validade do pedido (data_criacao)
+    // verificar se o link ja foi usado
+    date_default_timezone_set('America/Sao_Paulo');
+    $data = new DateTime('now');
+    $data_criacao = DateTime::createFromFormat(
+        'Y-m-d H:i:s',
+        $recuperar['data_criacao']
+    );
+    $UmDia = DateInterval::createFromDateString(('1 day'));
+    $dataExpiracao = date_add($data_criacao, $UmDia);
+    
+    if ($data < $dataExpiracao){
+        echo "Funcionou";
+    }
+}
